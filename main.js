3dea306757a276983ae0b30951dc8e981ea31f13
Rajan Midun Magar <43414436+rajanmidun@users.noreply.github.com>
2020-06-15 13:17:49 +0545

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
 // Destructuring is a JavaScript expression that makes it possible to unpack
 // values from arrays,or properties from objects, into distinct variables.
 
-//Array destructuring'
-// const names = ['ashish', 'suraj', 'rajan', 'tashi'];
-// // const friend1 = names[0];
-// // const friend2 = names[1];
+//Object destructuring
+const info = {
+  name: "Suraj",
+  address: "Kathmandu",
+  phoneNumber: "9834923423"
+}
+const { name: username, address, phoneNumber } = info;
 
-// const [friend1, , , friend4] = names;
-
-// console.log(friend1);
-// console.log(friend4);
-
-let a = 34;
-let b = 56;
-
-[b, a] = [a, b];
-console.log(a);
-console.log(b);
+console.log(username);
+console.log(address);
+console.log(phoneNumber);
