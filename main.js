3504577557c45e8961f74087e57ade831b4652da
Rajan Midun Magar <43414436+rajanmidun@users.noreply.github.com>
2020-06-15 12:58:35 +0545

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,14 @@
-//var
-// var name;
-// name = "Rajan";
-// console.log(name);
+// Template string or Template literal
+// before es6
+let firstName = "Rajan";
+let lastName = "Midun";
 
-//let  
-// let name
-// name = "Rajan";
-// console.log(name);
+// console.log(firstName + " \n" + lastName);
 
-//const
-const name;
-name = "Rajan";
-console.log(name);
-
-// function info() {
-//   if (true) {
-//     var name = "Rajan";
-//     console.log(name);
-//   }
-//   console.log(name1);
-// }
-// info();
+// // es6
+console.log(`My name is ${firstName} ${lastName}`);
+console.log(firstName.startsWith('R'));
+console.log(firstName.endsWith('d'));
+console.log(firstName.includes('jan'));
+console.log(firstName.repeat(12));
+// console.log(`Sum of 2 and 3 is ${2 + 3}`);
