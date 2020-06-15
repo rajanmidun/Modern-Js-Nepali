0143f9e5345f02dd93c3f655b36cb32533e82661
Rajan Midun Magar <43414436+rajanmidun@users.noreply.github.com>
2020-06-15 12:57:32 +0545

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+//var
+// var name;
+// name = "Rajan";
+// console.log(name);
+
+//let  
+// let name
+// name = "Rajan";
+// console.log(name);
+
+//const
+const name;
+name = "Rajan";
+console.log(name);
+
+// function info() {
+//   if (true) {
+//     var name = "Rajan";
+//     console.log(name);
+//   }
+//   console.log(name1);
+// }
+// info();
