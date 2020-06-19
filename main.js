b611960fdc304c31b436a2e884c7da550f335a40
Rajan Midun Magar <43414436+rajanmidun@users.noreply.github.com>
2020-06-19 11:56:57 +0545

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
-//-----------------REST PARAMETER--------------------
+//-----------------SPREAD OPERATOR--------------------
+//FOR ARRAY
+const data1 = [1, 2, 3, 4, 5, 6];
+const data2 = [7, 8, 9];
 
-function sum(...datas) {
-  console.log(datas);
-  let total = 0;
-  for (let i = 0; i < datas.length; i++) {
-    total += datas[i];
-  }
-  console.log(`SUm is ${total}`);
+console.log(...data1);
+//for concat
+console.log(data1.concat(data2));
+console.log([...data1, ...data2]);
+
+//for adding new elements
+console.log([...data1, ...data2, 34, 34, 34, 34]);
+
+//FOR OBJECT
+const obj1 = {
+  name: "rajan",
+  age: 23
 }
 
-sum(12, 13, 21323, 23, 23, 23, 23);
+const obj2 = {
+  address: "sfsf",
+  salary: 23
+}
+
+console.log({ ...obj1, ...obj2 });
+
+//for adding new properties
+console.log({ ...obj1, ...obj2, level: "bachelor" });
+
