13a1b5670e04ebebb23dae00155c0805e94350bd
Rajan Midun Magar <43414436+rajanmidun@users.noreply.github.com>
2020-06-19 11:22:17 +0545

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,8 @@
-//-----------------ARROW FUNCTION--------------------
-//-----------------BEFORE ES6
+//-----------------DEFAULT PARAMETER--------------------
 
-//FUNCTION DECLARATION
-function functionDeclaration() {
-  return "From Function Declaration";
-}
-console.log(functionDeclaration());
-
-
-//FUNCTION EXPRESSION
-const functionExpression = function () {
-  return 'From Function Expression'
-}
-console.log(functionExpression());
-
-
-//Moder Js/ ES6
-//IF ONLY ONE PARAMETER
-// const arrowFunction = (a) => `Number is ${a}`;
-// console.log(arrowFunction(12));
-
-//IF MULTIPLE PARAMETERS
-const arrowFunction = (a, b) => {
-  const sum = a + b;
-  return `Sum is ${sum}`;
+function sum(a = 12, b = 12) {
+  console.log(`Sum is ${a + b}`);
 }
 
-console.log(arrowFunction(12, 12));
+// sum();
+sum(13, 14);
